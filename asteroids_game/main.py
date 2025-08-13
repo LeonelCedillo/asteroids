bbc1b3e30095a89c2b574249ac555ba86764099e
LeonelCedillo <leonel.cs.979@gmail.com>
2025-08-13 11:59:52 -0700

    add circleshape.py

--- asteroids_game/main.py
+++ asteroids_game/main.py
@@ -1,15 +1,10 @@
-# this allows us to use code from the open-source pygame library
-# throughout this file
+# this allows us to use code from the open-source pygame library throughout this file
 import pygame
 from constants import *
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     clock = pygame.time.Clock()
     td = 0
 
@@ -20,7 +15,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        # tick(60): pause the game loop until 1/60th of a second has passed (60 fps).
+        # tick(60): pause the game loop for 1/60th of a second (60 FPS).
         td = clock.tick(60) / 1000 # from (16.67) milliseconds to seconds 
         
 
