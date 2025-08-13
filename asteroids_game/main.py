a566ffe693a29a2304d827fc69e19578cdd2d84a
LeonelCedillo <leonel.cs.979@gmail.com>
2025-08-13 12:49:13 -0700

    player rotate

--- asteroids_game/main.py
+++ asteroids_game/main.py
@@ -8,16 +8,18 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     td = 0
-    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
-        screen.fill("black")
-        player.draw(screen)
-        pygame.display.flip() # refresh the screen
         # This makes the window's close button work:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(td)
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip() # refresh the screen
+        
         # tick(60): pause the game loop for 1/60th of a second (60 FPS).
         td = clock.tick(60) / 1000 # from (16.67) milliseconds to seconds 
         
