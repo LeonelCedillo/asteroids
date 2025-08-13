9747a952b00331b378fb3a24705ba50326eb3afb
LeonelCedillo <leonel.cs.979@gmail.com>
2025-08-12 20:52:26 -0700

    add window painting black and closing properly

--- asteroids_game/main.py
+++ asteroids_game/main.py
@@ -1,9 +1,23 @@
 # this allows us to use code from the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        screen.fill("black")
+        pygame.display.flip() # refresh the screen
+        # This makes the window's close button work:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
 
 
 if __name__ == "__main__":
