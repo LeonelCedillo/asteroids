202963600c4152aa6d43de0838ad8a2e4c2a0c7a
LeonelCedillo <leonel.cs.979@gmail.com>
2025-08-12 19:36:31 -0700

    import pygame

--- asteroids_game/main.py
+++ asteroids_game/main.py
@@ -1,6 +1,15 @@
+# this allows us to use code from the open-source pygame library
+# throughout this file
+import pygame
+
 def main():
-    print("Hello from asteroids-game!")
+    print("Starting Asteroids!")
 
 
 if __name__ == "__main__":
     main()
+# "This line ensures the main() function is only called when this file is run directly; 
+# it won't run if it's imported as a module.
+# It's considered the "pythonic" way to structure an executable program in Python. 
+# Technically, the program will work fine by just calling main(), 
+# but you might get an angry letter from Guido van Rossum if you don't."
